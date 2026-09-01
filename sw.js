15e6b2b26f006eaccc81fabd55a2191b848845a1
chariotpower <chariotpower1@gmail.com>
2026-09-01 18:43:56 +0200

    Refresh offline cache for Snapshot 3.0

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='chariot-energy-snapshot-v2-20260901f';
-const ASSETS=['./','./index.html','./styles.css?v=20260901f','./i18n.js?v=20260901f','./app.js?v=20260901f','./chariot-logo.svg','./manifest.webmanifest','./icon-192.png','./icon-512.png','./og-image.png'];
+const CACHE='chariot-energy-snapshot-v3-20260901g';
+const ASSETS=['./','./index.html','./styles.css?v=20260901g','./i18n.js?v=20260901g','./app.js?v=20260901g','./chariot-logo.svg','./manifest.webmanifest','./icon-192.png','./icon-512.png','./og-image.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();if(new URL(event.request.url).origin===self.location.origin)caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(hit=>hit||caches.match('./index.html'))))});
