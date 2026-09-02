7e66cb8c6a3b83d366c760bd4ad30bfe0b038a58
chariotpower <chariotpower1@gmail.com>
2026-09-02 07:34:57 +0200

    Refresh offline cache for final geolocation repair

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='chariot-energy-snapshot-v3-20260902k';
-const ASSETS=['./','./index.html','./styles.css?v=20260902k','./i18n.js?v=20260902k','./app.js?v=20260902k','./analytics.js?v=20260902k','./chariot-logo.svg','./manifest.webmanifest','./icon-192.png','./icon-512.png','./og-image.png'];
+const CACHE='chariot-energy-snapshot-v3-20260902l';
+const ASSETS=['./','./index.html','./styles.css?v=20260902l','./i18n.js?v=20260902l','./app.js?v=20260902l','./analytics.js?v=20260902l','./chariot-logo.svg','./manifest.webmanifest','./icon-192.png','./icon-512.png','./og-image.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();if(new URL(event.request.url).origin===self.location.origin)caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(hit=>hit||caches.match('./index.html'))))});
