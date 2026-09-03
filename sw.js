b7d02dcc9af27cf4478d9e15ac50ee3bd3f3ee3b
chariotpower <chariotpower1@gmail.com>
2026-09-03 07:30:52 +0200

    Refresh application cache version

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='chariot-energy-snapshot-v4-20260902r';
-const ASSETS=['./','./index.html','./qualify.html','./application.html','./application.css?v=20260902r','./application.js?v=20260902r','./styles.css?v=20260902p','./i18n.js?v=20260902p','./app.js?v=20260902p','./analytics.js?v=20260902p','./chariot-logo.svg','./chariot-logo-transparent.png','./manifest.webmanifest','./icon-192.png','./icon-512.png','./og-image.png'];
+const CACHE='chariot-energy-snapshot-v4-20260903s';
+const ASSETS=['./','./index.html','./qualify.html','./application.html','./application.css?v=20260903s','./application.js?v=20260903s','./styles.css?v=20260902p','./i18n.js?v=20260902p','./app.js?v=20260902p','./analytics.js?v=20260902p','./chariot-logo.svg','./chariot-logo-transparent.png','./manifest.webmanifest','./icon-192.png','./icon-512.png','./og-image.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();if(new URL(event.request.url).origin===self.location.origin)caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(hit=>hit||caches.match('./index.html'))))});
